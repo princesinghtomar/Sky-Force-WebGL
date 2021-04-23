5854018bbe4b5a813f831b6c74df63b8cc9299a7
princesinghtomar <singhtomarprince470@gmail.com>
2021-04-24 02:07:25 +0530

    Player movement

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
-// We need 3 things everytime we use Three.js
-// Scene + Camera + Renderer
+// Some Important Variables and Functins
 pi = Math.PI
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+var rightPressed = false;
+var leftPressed = false;
+var upPressed = false;
+var downPressed = false;
+position_matrix = [0, -3, 0];
+rotation_matrix = [pi / 2, pi / 2, -pi / 2]
+
+// Scene + Camera + Renderer
+// We need 3 things everytime we use Three.js
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 const renderer = new THREE.WebGLRenderer({ antialias: true })
@@ -30,23 +40,71 @@ pointLight.position.set(25, 50, 25);
 scene.add(pointLight);
 
 const loader = new GLTFLoader();
-loader.load( '../models/spaceship3.glb', function ( gltf1 ) {
+loader.load('../models/spaceship3.glb', function (gltf1) {
 	playership = gltf1
 	console.log(playership)
-	gltf1.scene.position.set(0,-3,0)
-	gltf1.scene.rotation.set(pi/2,pi/2,-pi/2)
-	gltf1.scene.scale.set(0.3,0.3,0.3)
-	scene.add( gltf1.scene );
+	gltf1.scene.position.set(0, -3, 0)
+	gltf1.scene.rotation.set(pi / 2, pi / 2, -pi / 2)
+	gltf1.scene.scale.set(0.3, 0.3, 0.3)
+	scene.add(gltf1.scene);
 
-}, undefined, function ( error ) {
+}, undefined, function (error) {
 
-	console.error( error );
+	console.error(error);
 
-} );
+});
 
 function animate() {
 	requestAnimationFrame(animate)
 	// playership.scene.position.y -= 0.01
+
 	renderer.render(scene, camera)
 }
 animate()
+
+function keyDownHandler(event) {
+	if (event.keyCode == 39) {
+		rightPressed = true;
+		position_matrix[0] += 0.1;
+		if (playership.scene.position.x < 6.7) {
+			playership.scene.position.x += 0.1;
+		}
+	}
+	else if (event.keyCode == 37) {
+		leftPressed = true;
+		position_matrix[0] -= 0.1;
+		if (playership.scene.position.x > -6.7) {
+			playership.scene.position.x -= 0.1;
+		}
+	}
+	if (event.keyCode == 40) {
+		downPressed = true;
+		position_matrix[1] -= 0.1;
+		if (playership.scene.position.y > -3) {
+			playership.scene.position.y -= 0.1
+		}
+	}
+	else if (event.keyCode == 38) {
+		upPressed = true;
+		position_matrix[1] += 0.1;
+		if(playership.scene.position.y<3){
+			playership.scene.position.y += 0.1
+		}
+	}
+	console.log(position_matrix)
+}
+
+function keyUpHandler(event) {
+	if (event.keyCode == 39) {
+		rightPressed = false;
+	}
+	else if (event.keyCode == 37) {
+		leftPressed = false;
+	}
+	if (event.keyCode == 40) {
+		downPressed = false;
+	}
+	else if (event.keyCode == 38) {
+		upPressed = false;
+	}
+}
