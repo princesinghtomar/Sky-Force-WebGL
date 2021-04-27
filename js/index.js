1447cd1f38b0233ffc3f4e15420587fc840b6a3c
princesinghtomar <singhtomarprince470@gmail.com>
2021-04-27 08:03:35 +0530

    Added Some collision

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ enemy2_array = new Array();
 healthArr = new Array()
 var health = 10;
 var score = 0;
-var rockets = 5;
+var rockets = 10;
 var game_speed = 0.02;
 var run = true
 
@@ -76,7 +76,7 @@ function create_missiles() {
 
 // ... Change stars model ... 
 // ... Make Stars appear after Killing of an Enemy ...
-function create_stars(x,y) {
+function create_stars(x, y) {
 	const stloader = new GLTFLoader();
 	stloader.load("../models/star.glb", function (gltf) {
 		star_array.push(gltf);
@@ -89,12 +89,12 @@ function create_stars(x,y) {
 		console.error(error);
 	})
 }
-function stars_func(){
-	var number_of_stars = Math.round(Math.random()*5) + 1;
-	var val = game_length/number_of_stars
-	for (var i=0; i < number_of_stars-1;i++){
-		var put_star = Math.random()*12 - 6
-		create_stars(put_star,(i+1)*val)
+function stars_func() {
+	var number_of_stars = Math.round(Math.random() * 5) + 1;
+	var val = game_length / number_of_stars
+	for (var i = 0; i < number_of_stars - 1; i++) {
+		var put_star = Math.random() * 12 - 6
+		create_stars(put_star, (i + 1) * val)
 	}
 }
 stars_func()
@@ -102,18 +102,18 @@ stars_func()
 
 // ... Create Enemy on some Distance to each other ...
 // ... 
-function create_enemy(type,x,y) {
+function create_enemy(type, x, y) {
 	const enloader = new GLTFLoader();
 	var address_is = ""
-	if(type){
+	if (type) {
 		address_is = "../models/enemyspaceship.glb"
 	} else {
 		address_is = "../models/enemyspaceship2.glb"
 	}
 	enloader.load(address_is, function (gltf) {
-		if(type){
+		if (type) {
 			enemy1_array.push(gltf);
-		} else{
+		} else {
 			enemy2_array.push(gltf);
 		}
 		// console.log("Enemy : " + String(gltf));
@@ -129,18 +129,18 @@ function create_enemy(type,x,y) {
 create_enemy(1)
 
 function textload() {
-    var instance = new THREE.TextSprite({
-        alignment: 'left',
-        color: '#00ff00',
-        fontFamily: '"Times New Roman", Times, serif',
-        fontSize: 0.4,
-        fontStyle: 'bold',
-        text: [
-            'Life :'
-        ].join('\n'),
-    });
-    scene.add(instance);
-    return instance;
+	var instance = new THREE.TextSprite({
+		alignment: 'left',
+		color: '#00ff00',
+		fontFamily: '"Times New Roman", Times, serif',
+		fontSize: 0.4,
+		fontStyle: 'bold',
+		text: [
+			'Life :'
+		].join('\n'),
+	});
+	scene.add(instance);
+	return instance;
 }
 
 text1 = textload()
@@ -152,32 +152,41 @@ text2.text = "Score : " + String(score)
 text2.position.x = 0
 text2.position.y = -1.45
 
+text3 = textload()
+text3.text = "Missiles : " + String(rockets)
+text3.position.x = 6
+text3.position.y = -1.45
+
 function addCube() {
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    
-    cube.scale.set(0.1,0.1,0.1)
-    if (healthArr.length == 0) {
-        cube.position.set(-8.1, -1.6, 0)
-    }
-    else {
-        cube.position.x += healthArr[healthArr.length-1].position.x + 0.2
+	const geometry = new THREE.BoxGeometry();
+	const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+	const cube = new THREE.Mesh(geometry, material);
+
+	cube.scale.set(0.1, 0.1, 0.1)
+	if (healthArr.length == 0) {
+		cube.position.set(-8.1, -1.6, 0)
+	}
+	else {
+		cube.position.x += healthArr[healthArr.length - 1].position.x + 0.2
 		cube.position.y = -1.6
-    }
-    healthArr.push(cube)
-    scene.add(cube);
+	}
+	healthArr.push(cube)
+	scene.add(cube);
 }
 function makecube() {
-    for (let i = 0; i < health; i++) {
-        addCube()
-    }
+	for (let i = 0; i < health; i++) {
+		addCube()
+	}
 }
 makecube()
 
 function removeCube(index) {
-    healthArr[index].position.z = 100
-    healthArr.splice(index)
+	healthArr[index].position.z = 100
+	healthArr.splice(index)
+}
+
+function calculate_dist(x1, y1, x2, y2) {
+	return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))
 }
 
 function animate() {
@@ -188,27 +197,82 @@ function animate() {
 			player_missiles[i].scene.position.z = 100
 		}
 	}
-	if(camera.position.y < 30 + 1.4){
+	if (camera.position.y < 30 + 1.4) {
 		playership.scene.position.y += game_speed;
 		camera.position.y += game_speed;
-		text1.position.y += game_speed
-		text2.position.y += game_speed
-		for( i in healthArr){
+		text1.position.y += game_speed;
+		text2.position.y += game_speed;
+		text3.position.y += game_speed;
+		for (i in healthArr) {
 			healthArr[i].position.y += game_speed;
 		}
 	}
+	text3.text = "Missiles : " + String(rockets)
 	text2.text = "Score : " + String(score)
+	for (i in star_array) {
+		if (star_array[i].scene.position.z < 99) {
+			var dist = calculate_dist(
+				star_array[i].scene.position.x,
+				star_array[i].scene.position.y,
+				playership.scene.position.x,
+				playership.scene.position.y
+			)
+			if (dist < 1) {
+				star_array[i].scene.position.z = 100;
+				score += 100;
+			}
+		}
+	}
+	for (i in player_missiles) {
+		if (player_missiles[i].scene.position.z < 99) {
+			for (j in enemy1_array) {
+				if (enemy1_array[j].scene.position.z < 99) {
+					var dist = calculate_dist(
+						player_missiles[i].scene.position.x,
+						player_missiles[i].scene.position.y,
+						enemy1_array[j].scene.position.x,
+						enemy1_array[j].scene.position.y
+					);
+					if (dist < 0.5) {
+						enemy1_array[j].scene.position.z = 100;
+						player_missiles[i].scene.position.z = 100;
+						score += 50;
+					}
+				}
+			}
+		}
+	}
+	for (j in enemy1_array) {
+		if (enemy1_array[j].scene.position.z < 99) {
+			var dist = calculate_dist(
+				enemy1_array[j].scene.position.x,
+				enemy1_array[j].scene.position.y,
+				playership.scene.position.x,
+				playership.scene.position.y
+			);
+			if (dist < 1) {
+				if (health > 0) {
+					health -= 1;
+					removeCube(healthArr.length - 1);
+					enemy1_array[j].scene.position.z = 100;
+					score -= 20;
+				}
+			}
+		}
+	}
 	// if(health < 0){
 	// 	// Game Over
+	//	run = false
 	// 	return
 	// }
 	// if(playership.scene.position.y >= 30 && health > 0){
 	// 	// player Win
+	//	run = false
 	// 	return
 	// }
-	renderer.render(scene, camera)
+	renderer.render(scene, camera);
 }
-animate()
+animate();
 
 function keyDownHandler(event) {
 	let height = Window_height;
@@ -236,21 +300,21 @@ function keyDownHandler(event) {
 	}
 	else if (event.keyCode == 32) {
 		if (rockets > 0) {
-			create_missiles()
-			rockets -= 1
+			create_missiles();
+			rockets -= 1;
 		}
 	}
 	else if (event.keyCode == 70) {
-        if (health > 0) {
-            health -= 1
-            removeCube(healthArr.length-1)
-        }
-    }
-    else if(event.keyCode == 71){
-        health += 1
-        addCube()
-        // healthArr[healthArr.length-1].position.z = 100
-    }
+		if (health > 0) {
+			health -= 1;
+			removeCube(healthArr.length - 1);
+		}
+	}
+	else if (event.keyCode == 71) {
+		health += 1;
+		addCube();
+		// healthArr[healthArr.length-1].position.z = 100
+	}
 	console.log("playership.scene.position.y : " + String(playership.scene.position.y))
 	console.log("playership.scene.position.x : " + String(playership.scene.position.x))
 	console.log("camera.position.y : " + String(camera.position.y))
